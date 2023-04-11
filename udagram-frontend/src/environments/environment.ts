d811c06cfcd006f420241af57f98df5343fc8a50
quocdung.phi <quocdung.phi@gmail.com>
2023-04-11 22:05:51 +0700

    update deployment folder

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5d0802d4323b4b0581e869f37707565-706354118.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
