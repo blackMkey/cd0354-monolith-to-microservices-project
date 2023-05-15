8a9246c9f9003922ff86779899d70fd0a63eaa10
quocdung.phi <quocdung.phi@gmail.com>
2023-05-15 21:40:34 +0700

    update

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,8 @@
-# This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_DB=postgres
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=cloud-developer-database.cgsj0ur26vo1.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-# export AWS_BUCKET=arn:aws:s3:::cloud-developer-s3
-export AWS_BUCKET=cloud-developer-s3
+export POSTGRES_HOST=cloud-developer-database.cxpc0htc76q5.us-east-1.rds.amazonaws.com
+export AWS_BUCKET=cloud-developer-course-3-storage
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -5,7 +5,6 @@ import {sequelize} from './sequelize';
 import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
-import {config} from './config/config';
 import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 
@@ -20,9 +19,6 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS this lesson. It's
-  // something that will be covered in the next course.
   app.use(cors({
     allowedHeaders: [
       'Origin', 'X-Requested-With',
